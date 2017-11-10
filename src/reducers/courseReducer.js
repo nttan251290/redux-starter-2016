ed64c9d75a221ebba9a80c80449380ac5cb1515a
Nguyen Trong Tan <nttan251290@gmail.com>
2017-11-10 10:43:54 +0700

    - Fix bugs.

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -3,6 +3,7 @@ import initialState from './initialState';
 
 export default function courseReducer(state = initialState.courses, action) {
   switch(action.type) {
+
     case types.LOAD_COURSES_SUCCESS :
       return action.courses;
 
@@ -13,12 +14,13 @@ export default function courseReducer(state = initialState.courses, action) {
       ];
 
     case types.UPDATE_COURSE_SUCCESS :
-    return [
-      ...state.filter(course.id !== action.course.id),
-      Object.assign({}, action.course)
-    ];
+      return [
+        ...state.filter(course => course.id !== action.course.id),
+        Object.assign({}, action.course)
+      ];
 
     default:
       return state;
   }
+  
 }
